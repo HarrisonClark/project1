520083e375980fa0b31c7bf046d7e09fc4f5300a
harrison2013 <harrison.clark99@icloud.com>
2018-12-27 17:25:45 -0600

    added login and registration pages.

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session, render_template, request
+from flask import Flask, session, render_template, request, redirect
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -23,21 +23,44 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return render_template("book-reviews.html")
+    return redirect("/login")
 
-@app.route("/login", methods=["POST"])
+@app.route("/login")
 def login():
+    return render_template("login.html")
+
+@app.route("/process-login", methods=["POST"])
+def process_login():
     """Log into the Webpage"""
 
     # Get form information.
-    user = request.form.get("username")
+    username = request.form.get("username")
     password = request.form.get("password")
 
     # Make sure flight exists.
-    db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
-            {"username": user, "password": password})
+    if db.execute("SELECT userId FROM users WHERE username = :us AND password = :ps", {"us": username, "ps": password}).rowcount == 0:
+        return "Incorrect Username or Password."
+    else:
+        return redirect("/database")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/process-registration", methods=["POST"])
+def process_registration():
+    """Registers new account with Webpage"""
+
+    # Get form information.
+    usern = request.form.get("username")
+    passw = request.form.get("password")
+    print('----------------------------------------------------------')
+    print(usern, passw)
+    print('----------------------------------------------------------')
+    # Make sure flight exists.
+    db.execute("INSERT INTO users (username, password) VALUES (:username, :password)", {"username": usern, "password": passw})
     db.commit()
-    return "successful login"
+    return redirect("/database")
 
 @app.route("/database")
 def database():
@@ -46,3 +69,7 @@ def database():
     for user in users:
         text += str(user[0]) +": "+ user[1] +" "+ user[2] + "<br>"
     return 'database' + "<br>" + text
+
+@app.route("/user/<string:user_name>")
+def user(user_name):
+    return render_template("user-page.html", username=user_name)
