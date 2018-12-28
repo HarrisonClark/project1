637f2711333395c836f92cac85e62062b4335839
harrison2013 <harrison.clark99@icloud.com>
2018-12-27 20:20:39 -0600

    wrote code to import book.csv into SQL database

---
+++ import.py
@@ -0,0 +1,22 @@
+import csv
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL"))   # database engine object from SQLAlchemy that manages connections to the database
+                                                    # DATABASE_URL is an environment variable that indicates where the database lives
+db = scoped_session(sessionmaker(bind=engine))      # create a 'scoped session' that ensures different users' interactions with the
+                                                    # database are kept separate
+
+db.execute("CREATE TABLE books (id SERIAL PRIMARY KEY, isbn VARCHAR NOT NULL, title VARCHAR NOT NULL, author VARCHAR NOT NULL, year INTEGER NOT NULL, avg_rating NUMERIC DEFAULT 0, review_count NUMERIC DEFAULT 0);")
+db.commit()
+
+f = open("books.csv")
+reader = csv.reader(f)
+for isbn, title, author, year in reader: # loop gives each column a name
+    db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+        {"isbn": isbn, "title": title, "author": author, "year": int(float(year))})
+    # substitute values from CSV line into SQL command, as per this dict
+print('FINISHED************************************************************')
+db.commit() # transactions are assumed, so close the transaction finished
+f.close()
